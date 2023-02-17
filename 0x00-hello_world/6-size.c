7dffc6ce87c0b50cdc4b3d1885fbe39e2cf8dead
Lucky <luckysikolia7@gmail.com>
2023-02-16 17:46:30 -0800

    make betty syntax changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main (void) 
+/**
+ * main - A program that prints the size of various computer types
+ * Return: 0 (Success)
+ */
+int main(void)
 {
 	char a;
 	int b;
@@ -7,11 +11,10 @@ int main (void)
 	long int n;
 	long long int z;
 	
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)size of(a));
-	printf("Size of a int: %lu byte(s)\n", (unsigned long)size of(b));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)size of(f));
-	printf("Size of a long int n: %lu byte(s)\n", (unsigned long)size of(n));
-	printf("Size of a long long int z: %lu byte(s)\n", (unsigned long)size of(z));
-	
+	printf("Size of a char: %lu byte(s)\n",(unsigned long)size of(a));
+	printf("Size of a int: %lu byte(s)\n",(unsigned long)size of(b));
+	printf("Size of a float: %lu byte(s)\n",(unsigned long)size of(f));
+	printf("Size of a long int n: %lu byte(s)\n",(unsigned long)size of(n));
+	printf("Size of a long long int z: %lu byte(s)\n",(unsigned long)size of(z));
 	return 0;
 }
