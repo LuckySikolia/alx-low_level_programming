64f23d4b2fd4a929413d239d5d348f3097f32bad
Lucky <luckysikolia7@gmail.com>
2023-02-16 17:18:56 -0800

    c mandatory tasks

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (void) 
+{
+	char a;
+	int b;
+	float f;
+	long int n;
+	long long int z;
+	
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)size of(a));
+	printf("Size of a int: %lu byte(s)\n", (unsigned long)size of(b));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)size of(f));
+	printf("Size of a long int n: %lu byte(s)\n", (unsigned long)size of(n));
+	printf("Size of a long long int z: %lu byte(s)\n", (unsigned long)size of(z));
+	
+	return 0;
+}
