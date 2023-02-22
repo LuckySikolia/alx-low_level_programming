a94adffc8781ee5fbf8137134cedd557b68f2837
Lucky <luckysikolia7@gmail.com>
2023-02-21 16:20:00 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *main - print the _putchar statement followed by a new line
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char str[10]="_putchar";
+
+	int i;
+	for(i = 0; i < 8; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar(10);
+	return (0);
+}
