ce32106cf291b1dde8d69f4f8f6e5f9ff932d7b4
Lucky <luckysikolia7@gmail.com>
2023-02-23 15:30:10 -0800

    betty changes

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,13 +7,13 @@
 *
 *
 *Return: Always 0 (Success)
-**/
+*/
 
 void print_numbers(void)
 {
 	char num;
-
-	for (digits = '0'; digits <= '9'; digits++)
+	
+	for (num = '0'; num <= '9'; num++)
 	{
 		_putchar(num);
 	}
