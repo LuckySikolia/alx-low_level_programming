8fdec71f4f956682b400cd239fe55a52e01f2ec7
Lucky <luckysikolia7@gmail.com>
2023-02-23 15:45:53 -0800

    betty changes

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-*print_numbers - print numbers from 0 to 9 followed by a new line
-*
-*
-*
-*Return: Always 0 (Success)
-*/
-
+ * print_numbers - Prints the numbers, from 0 to 9, followed by a new line
+ *
+ *
+ * Return: is zero
+ */
 void print_numbers(void)
 {
 	char num;
-	
-	for (num = '0'; num <= '9'; num++)
+
+	for (num = '0'; num <= '9'; num)
 	{
 		_putchar(num);
 	}
