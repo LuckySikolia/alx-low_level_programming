ae80dbb485cf81fda9b01ca99066c6b0532e6d89
Lucky <luckysikolia7@gmail.com>
2023-02-23 15:10:03 -0800

    task 0 - 5

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*mul- multiplies two integers
+*
+*@a: first integer
+*@b: second integer
+*
+*Return: result of a * b
+**/
+
+int _isdigit(int a, int b)
+{
+	if ((a >= '0' && a <= '9') || (b >= '0' && b <= '9'))
+	{
+		a * b;
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*print_numbers - print numbers from 0 to 9 followed by a new line
+*
+*
+*
+*Return: Always 0 (Success)
+**/
+
+void print_numbers(void)
+{
+	char num;
+	
+	while (num >= '0' && num <= '9')
+	{
+		num++;
+		_putchar (num);
+	}
+	_putchar (10);
+}
