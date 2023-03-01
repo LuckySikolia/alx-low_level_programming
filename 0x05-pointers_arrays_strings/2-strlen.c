5db533775cfca1e194279cabbcfbb3a156db9ae7
Lucky <luckysikolia7@gmail.com>
2023-02-28 17:44:06 -0800

    fix trailing errors

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,17 @@
 #include <string.h>
 
 /**
- * _strlen - function that returns the length of a string
- * @s: string input
+ * _strlen - Function that returns the length of a string
  *
- * Return : length.
+ * @s: this is the input string
+ *
+ * Return: Lenngth of the string
  */
 
 int _strlen(char *s)
 {
 	int length = 0;
-	
+
 	while (*s != '\0')
 	{
 		length++;
