c363bc63961244deb5a54f3a6d84bd8b59bfa41b
Lucky <luckysikolia7@gmail.com>
2023-02-28 17:10:54 -0800

    first 3 tasks

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  swap_int - function that that swaps the values of two integer
+ * @a: The first character to enter the program
+ * @b: The second character to enter the program
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swapping;
+	
+	swapping = *a;
+	*a = *b;
+	*b = temp;
+}
