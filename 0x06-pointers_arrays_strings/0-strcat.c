30b9abdda13638bf6f120b2337556dcc1f62b455
Lucky <luckysikolia7@gmail.com>
2023-03-03 14:31:26 -0800

    fixed betty issues

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,26 +1,20 @@
 #include "main.h"
-
 /**
- * *_strcat - concatenates two strings
- * @dest: characters to be appended to
- * @src:  characters to append to dest
- * Return: Always 0
+ * _strcat - concatenates two strings
+ * @src: character to append to dest
+ * @dest: character to be appended to
+ * Return: 0 always success
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i, j;
 
-	for (i = 0; dest[i] != '\0')
-	{
-		i++;
-	}
-	
+	for (i = 0; dest[i] != '\0'; i++)
+		;
 	for (j = 0; src[j] != '\0'; i++, j++)
 	{
 		dest[i] = src[j];
 	}
-
 	return (dest);
 }
