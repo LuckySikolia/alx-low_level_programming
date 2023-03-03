6dd86a38f5d3e30ce957ecf8108faf6bc77bc68c
Lucky <luckysikolia7@gmail.com>
2023-03-03 13:07:04 -0800

    fix betty errors

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,12 +11,12 @@ char *_strcat(char *dest, char *src)
     int i;
     int j;
 
-    for (i=0; dest[i] != '\0'; i++);
+    for (i = 0; dest[i] != '\0'; i++);
 
     for (j = 0; src[j] != '\0'; i++, j++)
     {
         dest[i] = src[j];
     }
-
-    return(dest);
+    
+    return (dest);
 }
