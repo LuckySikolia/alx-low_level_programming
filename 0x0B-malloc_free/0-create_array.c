5a4525e97222827d6fd08da0b6c5728c470ad54e
Lucky <luckysikolia7@gmail.com>
2023-03-15 07:26:48 -0700

    fix segmentation fault error

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,21 +7,21 @@
  * @size: the desired size of the array
  * Return: a pointer to the array, or NULL if it fails or is = 0
  */
-
 char *create_array(unsigned int size, char c)
 {
-	char *arr;
-	unsigned int i = 0;
+	char *array = NULL;
+	unsigned int i;
 
 	if (size == 0)
-	{
 		return (NULL);
-	}
-	arr = (char *) malloc(size * sizeof(char));
-	while (i < size)
+	if (size != 0)
 	{
-		arr[i] = c;
-		i++;
+		array = (char *)malloc(size * sizeof(char));
+		if (array != NULL)
+		{
+			for (i = 0; i < size; i++)
+				array[i] = c;
+		}
 	}
-	return (arr);
+	return (array);
 }
