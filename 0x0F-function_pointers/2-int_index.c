c3293e04f662bb32a662e740a13e9f1636ca4e02
Lucky <luckysikolia7@gmail.com>
2023-03-23 11:58:39 -0700

    changs

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,28 +1,26 @@
 #include "function_pointers.h"
 
 /**
- * int_index - return the index of the first element
- * @size: no of array elements in array
- * @array: Pointer array
- * @cmp: is a pointer to the function used to compre values
- * Return: -1 if no match, -1 if size <=0, first element
-*/
+ * int_index - search for an integer
+ * @array: integer array
+ * @size: array size
+ * @cmp: pointer to the function to be used
+ * Return: index of first element
+ */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
+	int i;
 
-	if (size <= 0)
+	if (array && cmp)
 	{
-		return (-1);
+		if (size <= 0)
+			return (-1);
+
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+				return (i);
 	}
 
-	for (; i <= size; i++)
-	{
-		if (cmp(array[i]) != 0)
-		{
-			return (i);
-		}
-	}
 	return (-1);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,31 +1,41 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - perform simple operations
- * @argc: no of arguments
- * @argv: array of arguments
- * Return: Always 0 success
-*/
-int main(int argc, int *argv[])
+ * main - Main ENtry
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
 {
-	int (*opr)(int, int);
+	int a, b;
+	int (*operation)(int, int);
 
 	if (argc != 4)
 	{
-		printf("Error \n");
+		printf("Error\n");
 		exit(98);
 	}
 
-	opr = get_op_func(argv[2]);
-
-	if (!opr)
+	if (argv[2][1])
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	printf("%d\n", opr(atoi(argv[1], atoi(argv[3])));
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", operation(a, b));
+
 	return (0);
 }
