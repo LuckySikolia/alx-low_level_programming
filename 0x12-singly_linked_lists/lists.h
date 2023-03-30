2ea3a1dcbd92c2639a453607554e19bce32ec197
Lucky <luckysikolia7@gmail.com>
2023-03-30 15:36:20 -0700

    fix betty in header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,15 +8,14 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- * 
  * Description: singly linked list node structure
 */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-}list_t;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 /* Function prototypes */
 int _putchar(char c);
